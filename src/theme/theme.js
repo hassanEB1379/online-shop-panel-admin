0e15080dec856c759ac11343ea633e9d9e8b39b8
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-03 15:30:25 +0430

    add custom typography

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,7 +1,54 @@
 import { createMuiTheme } from '@material-ui/core';
 
 export const theme = createMuiTheme({
+  direction: 'rtl',
   typography: {
     fontFamily: 'Vazir',
+    h1: {
+      fontSize: 36,
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: 32,
+      fontWeight: 700,
+    },
+    h3: {
+      fontSize: 28,
+      fontWeight: 700,
+    },
+    h4: {
+      fontSize: 24,
+      fontWeight: 700,
+    },
+    h5: {
+      fontSize: 20,
+      fontWeight: 700,
+    },
+    h6: {
+      fontSize: 18,
+      fontWeight: 700,
+    },
+    caption: {
+      fontSize: 14,
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        borderRadius: '4rem',
+        padding: '.5rem 1.5rem',
+      },
+    },
+    MuiPaper: {
+      rounded: {
+        borderRadius: '1rem',
+      },
+    },
+    MuiOutlinedInput: {
+      root: {
+        borderRadius: '4rem',
+        padding: '0 1rem',
+      },
+    },
   },
 });
