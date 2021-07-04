fe0e37a67d4af6b06bec5b82b28ccfcb2f5bca5e
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-04 14:24:52 +0430

    add panel layout route

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import {
   Redirect,
 } from 'react-router-dom';
 import LoginLayout from 'layouts/LoginLayout';
+import PanelLayout from 'layouts/PanelLayout';
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route path="/panel" component={PanelLayout} />
         <Route path="/login" component={LoginLayout} />
         <Redirect from="/" to="/login" />
       </Switch>
