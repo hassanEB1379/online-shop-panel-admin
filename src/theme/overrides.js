45dcf3a0f35174d577a7e942602de26a9f3bc2fa
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-04 14:26:51 +0430

    write overrides in separate file

---
+++ src/theme/overrides.js
@@ -0,0 +1,35 @@
+const overrides = {
+  MuiButton: {
+    root: {
+      borderRadius: '4rem',
+      padding: '.5rem 1.5rem',
+    },
+  },
+  MuiPaper: {
+    rounded: {
+      borderRadius: '1rem',
+    },
+  },
+  MuiOutlinedInput: {
+    root: {
+      borderRadius: '4rem',
+      padding: '0 1rem',
+    },
+  },
+  MuiAppBar: {
+    colorPrimary: {
+      color: '#000',
+      backgroundColor: '#fff',
+    },
+  },
+  MuiDivider: {
+    vertical: {
+      margin: '0 10px',
+    },
+    root: {
+      backgroundColor: 'rgb(0 0 0 / 6%)',
+    },
+  },
+};
+
+export default overrides;
