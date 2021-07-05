e2dd5fad5650624a44b16770bbe7f457c6ea4be6
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-05 12:34:35 +0430

    create sidebar component

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,73 @@
+import {
+  Avatar,
+  Divider,
+  Drawer,
+  List,
+  ListItemText,
+  ListItemIcon,
+  ListItem,
+  Typography,
+  Grid,
+  Box,
+} from '@material-ui/core';
+
+// icons
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+
+// styles
+import useStyles from './styles';
+
+const drawerWidth = 255;
+
+const Sidebar = ({ open }) => {
+  const classes = useStyles({ drawerWidth });
+
+  return (
+    <Drawer
+      variant="persistent"
+      anchor="left"
+      open={open}
+      PaperProps={{
+        style: {
+          width: drawerWidth,
+        },
+      }}
+    >
+      <div className={classes.drawerHeader}></div>
+      <Divider />
+      {/* list-profile */}
+      <Box padding="15px">
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <Avatar className={classes.profileImg} />
+          </Grid>
+
+          <Grid item>
+            <Typography variant="h5" align="center">
+              اسم ادمین
+            </Typography>
+          </Grid>
+
+          <Grid item>
+            <Typography align="center"> خوش آمدید</Typography>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Divider />
+
+      <List>
+        {['رسانه'].map((text, index) => (
+          <ListItem button key={index}>
+            <ListItemIcon>
+              <HomeRoundedIcon />
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
