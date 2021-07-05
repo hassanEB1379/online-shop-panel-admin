68f0c254274c1eeeff639760d64657556405de6d
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-05 12:33:33 +0430

    add styles to file uploader

---
+++ src/components/fileUploader/styles.js
@@ -0,0 +1,36 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  uploadField: {
+    padding: theme.spacing(1),
+    position: 'relative',
+    height: 250,
+    border: '2px dashed',
+    borderColor: theme.palette.primary.main,
+  },
+  fileInput: {
+    width: ' 100%',
+    opacity: 0,
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    cursor: 'pointer',
+    zIndex: theme.zIndex.modal,
+  },
+  tileContainer: {
+    position: 'relative',
+    zIndex: theme.zIndex.tooltip,
+  },
+  tile: {
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: ' 100%',
+  },
+  removeBtn: {
+    position: 'absolute',
+    top: 0,
+    color: '#fff',
+  },
+}));
+
+export default useStyles;
