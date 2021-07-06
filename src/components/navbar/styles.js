343ff3e3da9267d7d766b63241665fd4040a760f
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-06 11:49:33 +0430

    add responsive styles

--- src/components/navbar/styles.js
+++ src/components/navbar/styles.js
@@ -3,22 +3,31 @@ import { makeStyles } from '@material-ui/core';
 const useStyles = makeStyles(theme => ({
   appbar: {
     height: 80,
+    [theme.breakpoints.down('xs')]: {
+      height: 60,
+    },
     boxShadow: theme.shadows[1],
     justifyContent: 'center',
   },
-  collaps: {
+  toolbar: {
+    zIndex: theme.zIndex.appBar,
+    backgroundColor: '#fff',
+  },
+  navMenu: {
     position: 'absolute',
     width: '100%',
+    height: '100%',
     right: 0,
-    top: 70,
+    top: 60,
     backgroundColor: '#fff',
-    padding: 20,
-    zIndex: 900,
+    padding: '5px 20px',
+    zIndex: theme.zIndex.speedDial,
+    boxShadow: theme.shadows[1],
     transition: 'transform .2s ease-out',
     transform: props =>
       props.mobileNavOpen
-        ? 'translate3d(0 , -80px , 0)'
-        : 'translate3d(0 , 0 , 0)',
+        ? 'translate3d(0 , 0 , 0)'
+        : 'translate3d(0 , -60px , 0)',
   },
 }));
 
