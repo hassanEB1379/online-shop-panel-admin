3b9de47c02febae9f3761f04bfc8eb98848d766c
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-06 11:53:15 +0430

    responsive filelist

--- src/pages/media/components/FileList.js
+++ src/pages/media/components/FileList.js
@@ -43,7 +43,7 @@ const FileList = () => {
           <Typography variant="h4">فایل های آپلود شده</Typography>
         </Grid>
 
-        <Grid item>
+        <Grid item xs={12}>
           <TableContainer>
             <Table>
               <TableHead>
