5ac9ac0c3a101ad1233f7e51fb7228bc97df6f7b
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-13 17:48:15 +0430

    some change in media context

--- src/contexts/MediaContext.js
+++ src/contexts/MediaContext.js
@@ -1,8 +1,9 @@
+import { useGetMedia } from 'hooks/MediaHooks';
 import { createContext, useContext, useReducer } from 'react';
 
 const MediaContext = createContext();
 
-const useMedia = () => {
+const useMediaContext = () => {
   const media = useContext(MediaContext);
 
   if (media === undefined) {
@@ -14,8 +15,11 @@ const useMedia = () => {
 
 const mediaReducer = (media, action) => {
   switch (action.type) {
+    case 'GET_FILES': {
+      return [...action.payload];
+    }
     case 'UPLOAD': {
-      return;
+      return [action.payload, ...media];
     }
     case 'DELETE': {
       return;
@@ -27,41 +31,12 @@ const mediaReducer = (media, action) => {
   }
 };
 
-const initialMedia = [
-  {
-    id: 1,
-    thumbnail: '/static/images/camera.jpg',
-    name: 'camera.jpg',
-    url: 'http://localhost:3000/static',
-  },
-  {
-    id: 2,
-    thumbnail: '/static/images/ben.jpeg',
-    name: 'ben.jpg',
-    url: 'http://localhost:3000/static',
-  },
-  {
-    id: 3,
-    thumbnail: '/static/images/download.jpg',
-    name: 'download.jpg',
-    url: 'http://localhost:3000/static',
-  },
-  {
-    id: 4,
-    thumbnail: '/static/images/images (1).jpg',
-    name: 'images (1).jpg',
-    url: 'http://localhost:3000/static',
-  },
-  {
-    id: 5,
-    thumbnail: '/static/images/city.jpg',
-    name: 'city.jpg',
-    url: 'http://localhost:3000/static',
-  },
-];
-
 const MediaProvider = ({ children }) => {
-  const [media, dispatch] = useReducer(mediaReducer, initialMedia);
+  const [media, dispatch] = useReducer(mediaReducer, []);
+
+  useGetMedia(data => {
+    dispatch({ type: 'GET_FILES', payload: data });
+  });
 
   return (
     <MediaContext.Provider value={{ media, dispatch }}>
@@ -70,5 +45,5 @@ const MediaProvider = ({ children }) => {
   );
 };
 
-export { useMedia };
+export { useMediaContext };
 export default MediaProvider;
