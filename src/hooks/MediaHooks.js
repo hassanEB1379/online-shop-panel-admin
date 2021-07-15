f0bf2f54ccf8f6f0e2db29f0b2ddfc88d2d28144
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-15 17:07:38 +0430

    get and update media hook

--- src/hooks/MediaHooks.js
+++ src/hooks/MediaHooks.js
@@ -1,4 +1,17 @@
 import { uploadSingleFile } from 'api/MediaApi';
-import { useState } from 'react';
+import { getMedia } from 'api/MediaApi';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 
-export const useUploadFile = () => {};
+export function useAllMedia() {
+  return useQuery('media', getMedia, { retry: false });
+}
+
+export function useUploadFile() {
+  const queryClient = useQueryClient();
+
+  return useMutation(uploadSingleFile, {
+    onSuccess: () => {
+      queryClient.refetchQueries('media');
+    },
+  });
+}
