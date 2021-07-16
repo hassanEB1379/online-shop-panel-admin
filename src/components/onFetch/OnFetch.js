52df5c8f1e5747ae1ac3aaf59fa4023631d4e876
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-16 17:21:51 +0430

    create onFetch component

---
+++ src/components/onFetch/OnFetch.js
@@ -0,0 +1,42 @@
+import {
+  Box,
+  Grid,
+  Typography,
+  IconButton,
+  CircularProgress,
+} from '@material-ui/core';
+import { RepeatRounded } from '@material-ui/icons';
+
+const OnFetch = ({ variant, reFetch, ...rest }) => {
+  const handleRefetch = () => reFetch();
+
+  return (
+    <Box bgcolor="#fff" height="300px" clone {...rest}>
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        spacing={4}
+      >
+        <Grid item>
+          {variant === 'error' && (
+            <IconButton color="primary" onClick={handleRefetch}>
+              <RepeatRounded />
+            </IconButton>
+          )}
+
+          {variant === 'loading' && <CircularProgress />}
+        </Grid>
+
+        <Grid item>
+          <Typography>
+            {variant === 'error' ? 'خطا در برقراری ارتباط' : 'در حال بارگذاری'}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default OnFetch;
