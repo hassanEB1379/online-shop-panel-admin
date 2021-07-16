4912b08ed52f8b8e49e3f289e206529da56ab7c8
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-16 17:24:13 +0430

    using onFetch component in addNewFiles

--- src/pages/media/components/AddNewFiles.js
+++ src/pages/media/components/AddNewFiles.js
@@ -1,16 +1,35 @@
-import { Button, Grid, Paper, Typography } from '@material-ui/core';
+import { Button, Grid, Paper, Typography, makeStyles } from '@material-ui/core';
 import { useUploadFile } from 'hooks/MediaHooks';
 import FileUploader from 'components/fileUploader/FileUploader';
 import { useState } from 'react';
+import OnFetch from 'components/onFetch/OnFetch';
 
+// constants
 const BTN_TEXT = 'آپلود فایل جدید';
+const BTN_TEXT_ERROR = 'تلاش مجدد';
+
+// styles
+const useStyles = makeStyles(theme => ({
+  loading: {
+    position: 'absolute',
+    inset: 0,
+    opacity: '.9',
+    zIndex: theme.zIndex.tooltip,
+  },
+  loadingContainer: {
+    position: 'relative',
+    overflow: 'hidden',
+  },
+}));
 
 const AddNewFiles = () => {
+  const classes = useStyles();
+
   const [file, setFile] = useState();
 
   const handleFileUploaderChange = newFiles => setFile(newFiles);
 
-  const { mutate: upload } = useUploadFile();
+  const { mutate: upload, isLoading, isError } = useUploadFile();
 
   const handleUpload = () => upload(file);
 
@@ -21,18 +40,22 @@ const AddNewFiles = () => {
           <Typography variant="h4"> آپلود فایل جدید</Typography>
         </Grid>
 
-        <Grid item>
+        <Grid item className={classes.loadingContainer}>
+          {isLoading && (
+            <OnFetch variant="loading" className={classes.loading} />
+          )}
+
           <FileUploader maxSize="6000000" onUpload={handleFileUploaderChange} />
         </Grid>
 
         <Grid item>
           <Button
             onClick={handleUpload}
-            disabled={!file}
+            disabled={!file || isLoading}
             color="primary"
             variant="contained"
           >
-            {BTN_TEXT}
+            {isError || isLoading ? BTN_TEXT_ERROR : BTN_TEXT}
           </Button>
         </Grid>
       </Grid>
