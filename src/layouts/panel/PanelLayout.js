2589cb0e10c222b1cc770364f22259c0b5792d21
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-24 21:49:11 +0430

    using useRoutes hook for render routes

--- src/layouts/panel/PanelLayout.js
+++ src/layouts/panel/PanelLayout.js
@@ -1,17 +1,19 @@
 import { Grid } from '@material-ui/core';
 import { useState } from 'react';
 import Navbar from 'components/navbar/Navbar';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Media from 'pages/media/Media';
+import { Switch, Redirect } from 'react-router-dom';
 import Sidebar from 'components/sidebar/Sidebar';
-import AddProduct from 'pages/addProduct/AddProduct';
-import ProductList from 'pages/ProductList/ProductList';
-import Settings from 'pages/settings/Settings';
+import { useRoutes, useRouteName } from 'hooks/RoutesHooks';
+import PageTitle from 'components/pageTitle/PageTitle';
 
 // styles
 import useStyles from './styles';
 
-const PanelLayout = () => {
+const PanelLayout = ({ location }) => {
+  const routes = useRoutes();
+
+  const routeName = useRouteName(location.pathname);
+
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   const classes = useStyles({ sidebarOpen });
@@ -26,15 +28,13 @@ const PanelLayout = () => {
       <Sidebar open={sidebarOpen} />
 
       <Grid item className={classes.contentWrapper}>
+        <PageTitle title={routeName} />
+
         <Switch>
-          <Route path="/panel/media" component={Media} />
-          <Route path="/panel/settings" component={Settings} />
-          <Route path="/panel/product/add-product" component={AddProduct} />
-          <Route path="/panel/product/product-list" component={ProductList} />
+          {routes}
 
           {/* Redirects */}
           <Redirect from="/panel" to="/panel/dashboard" />
-          <Redirect from="/panel/product" to="/panel/product/add-product" />
         </Switch>
       </Grid>
     </Grid>
