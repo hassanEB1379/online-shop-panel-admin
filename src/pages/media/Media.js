0cfce58570aa05842507c998b2ac359b2f7e0bc9
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-07-24 21:51:15 +0430

    remove pageTitle component

--- src/pages/media/Media.js
+++ src/pages/media/Media.js
@@ -1,15 +1,10 @@
 import { Grid } from '@material-ui/core';
-import PageTitle from 'components/pageTitle/PageTitle';
 import AddNewFiles from './components/AddNewFiles';
 import FileList from './components/FileList';
 
 const Media = () => {
   return (
     <Grid container direction="column" spacing={4}>
-      <Grid item xs={12}>
-        <PageTitle title="رسانه" subTitle="مدیریت فایل ها" />
-      </Grid>
-
       <Grid item xs={12}>
         <AddNewFiles />
       </Grid>
