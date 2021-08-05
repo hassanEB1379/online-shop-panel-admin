b0084d3eb0a0e2ff007651fa45a85eb52d2774a2
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-05 09:11:54 +0430

    change color of sidebar

--- src/components/sidebar/components/sidebarLink/styles.js
+++ src/components/sidebar/components/sidebarLink/styles.js
@@ -2,19 +2,20 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   mainItem: {
+    color: theme.palette.text.sidebar,
     transition: theme.transitions.create('margin-left', {
       easing: theme.transitions.easing.easeIn,
       duration: theme.transitions.duration.shortest,
     }),
     '&:hover': {
       marginLeft: 3,
-      color: theme.palette.primary.main,
+      color: theme.palette.text.hover,
     },
   },
   subItem: {
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.sidebar,
     '&:hover': {
-      color: theme.palette.primary.main,
+      color: theme.palette.text.hover,
     },
   },
   text: {
