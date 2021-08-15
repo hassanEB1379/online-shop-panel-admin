e5c20732abd2909d126585dc601fd2032edb807f
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-15 18:46:20 +0430

    replace routes with Private and Public route components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import LoginLayout from 'layouts/LoginLayout';
 import PanelLayout from 'layouts/panel/PanelLayout';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import PrivateRoute from 'components/routes/PrivateRoute';
+import PublicRoutes from 'components/routes/PublicRoutes';
 
 const client = new QueryClient();
 
@@ -16,8 +13,8 @@ const App = () => {
     <QueryClientProvider client={client}>
       <Router>
         <Switch>
-          <Route path="/panel" component={PanelLayout} />
-          <Route path="/login" component={LoginLayout} />
+          <PrivateRoute path="/panel" component={PanelLayout} />
+          <PublicRoutes restricted path="/login" component={LoginLayout} />
           <Redirect from="/" to="/login" />
         </Switch>
       </Router>
