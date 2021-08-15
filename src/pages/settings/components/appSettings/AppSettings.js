ea145f9d89149609c484ea23d77a79f554254ab4
hassanEB1379 <hassanebrahimi560@gmail.com>
2021-08-15 18:46:58 +0430

    clean app settings component

--- src/pages/settings/components/appSettings/AppSettings.js
+++ src/pages/settings/components/appSettings/AppSettings.js
@@ -1,100 +1,35 @@
-import { Box, Grid, Typography } from '@material-ui/core';
-import { Brightness2, WbSunny } from '@material-ui/icons';
-import { useThemeInfoDispatch, useThemeInfo } from 'contexts/ThemeContext';
+import { Button, Grid } from '@material-ui/core';
+import { useUserDispatch } from 'contexts/UserContext';
 import { useTranslation } from 'react-i18next';
-import { changeDir, changeTheme } from 'reducers/ThemeReducer';
-import StyledRadio from './components/StyledRadio';
-
-// styles
-import useStyles from './styles';
-
-// palettes
-import { blue, purple, red } from '@material-ui/core/colors';
+import { logout } from 'reducers/UserReducer';
+import Langs from './components/Langs';
+import Themes from './components/Themes';
 
 const AppSettings = () => {
-  const classes = useStyles();
+  const { t } = useTranslation();
 
-  const { t, i18n } = useTranslation();
+  const dispatch = useUserDispatch();
 
-  const dispatch = useThemeInfoDispatch();
-  const { themeName } = useThemeInfo();
-
-  const switchLang = e => {
-    const lang = e.target.value;
-    const langDir = i18n.dir(lang);
-
-    i18n.changeLanguage(lang);
-    dispatch(changeDir(langDir));
+  const handleLogout = () => {
+    dispatch(logout());
   };
 
-  const switchTheme = e => dispatch(changeTheme(e.target.value));
-
   return (
-    <Box p="2rem 0">
-      <Grid container direction="column" spacing={2}>
-        <Grid item>
-          <Typography>{t('setting.changeTheme')}</Typography>
-        </Grid>
-
-        <Grid item container onChange={switchTheme}>
-          <StyledRadio
-            defaultChecked={themeName === 'light'}
-            value="light"
-            name="theme"
-            className={classes.light}
-            icon={<WbSunny htmlColor="#ffca0a" />}
-          />
-
-          <StyledRadio
-            defaultChecked={themeName === 'dark'}
-            value="dark"
-            name="theme"
-            className={classes.dark}
-            icon={<Brightness2 htmlColor="#fff" />}
-          />
-
-          <StyledRadio
-            defaultChecked={themeName === 'blue'}
-            value="blue"
-            name="theme"
-            palette={blue}
-          />
-
-          <StyledRadio
-            defaultChecked={themeName === 'red'}
-            value="red"
-            name="theme"
-            palette={red}
-          />
-
-          <StyledRadio
-            defaultChecked={themeName === 'purple'}
-            value="purple"
-            name="theme"
-            palette={purple}
-          />
-        </Grid>
-
-        <Grid item>
-          <Typography>{t('setting.changeLang')}</Typography>
-        </Grid>
-
-        <Grid item container onChange={switchLang}>
-          <StyledRadio
-            defaultChecked={i18n.language === 'fa'}
-            value="fa"
-            name="lang"
-            flag="/static/images/iran.png"
-          />
-          <StyledRadio
-            defaultChecked={i18n.language === 'en'}
-            value="en"
-            name="lang"
-            flag="/static/images/united-kingdom.png"
-          />
-        </Grid>
+    <Grid container direction="column" spacing={4}>
+      <Grid item>
+        <Themes />
       </Grid>
-    </Box>
+
+      <Grid item>
+        <Langs />
+      </Grid>
+
+      <Grid item>
+        <Button onClick={handleLogout} color="primary" variant="contained">
+          {t('setting.logout')}
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
